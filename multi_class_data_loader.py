dc1aeb45b0d3dd37d1078594b380d7659510a288
MyoungEun Koh <45139611+meunKoh@users.noreply.github.com>
2020-07-02 17:43:44 +0900

    데이터 정제

--- multi_class_data_loader.py
+++ multi_class_data_loader.py
@@ -24,9 +24,9 @@ class MultiClassDataLoader(object):
 
 
     def define_flags(self):
-        self.__flags.DEFINE_string("train_data_file", "./data/bh_big.train", "Data source for the training data.")
-        self.__flags.DEFINE_string("dev_data_file", "./data/bh_big.dev", "Data source for the cross validation data.")
-        self.__flags.DEFINE_string("test_data_file", "./data/bh_big.test", "Data source for the test data.")
+        self.__flags.DEFINE_string("train_data_file", "./data/bh.good3.train", "Data source for the training data.")
+        self.__flags.DEFINE_string("dev_data_file", "./data/bh.good3.dev", "Data source for the cross validation data.")
+        self.__flags.DEFINE_string("test_data_file", "./data/bh.good3.test", "Data source for the test data.")
         self.__flags.DEFINE_string("class_data_file", "./data/bh_big.cls", "Data source for the class list.")
 
     def prepare_data(self):
