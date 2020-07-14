2f62aac165efa8cbe5221d5d310c0d32e63fdafc
MyoungEun Koh <45139611+meunKoh@users.noreply.github.com>
2020-07-14 10:57:01 +0900

    sp30k

--- multi_class_data_loader.py
+++ multi_class_data_loader.py
@@ -25,8 +25,8 @@ class MultiClassDataLoader(object):
 
     def define_flags(self):
         self.__flags.DEFINE_string("train_data_file", "./data/bh.good3.sp30k.train", "Data source for the training data.")
-        self.__flags.DEFINE_string("dev_data_file", "./data/bh.good13.sp30k.dev", "Data source for the cross validation data.")
-        self.__flags.DEFINE_string("test_data_file", "./data/bh.good13.sp30k.test", "Data source for the test data.")
+        self.__flags.DEFINE_string("dev_data_file", "./data/bh.good3.sp30k.dev", "Data source for the cross validation data.")
+        self.__flags.DEFINE_string("test_data_file", "./data/bh.good3.sp30k.test", "Data source for the test data.")
         self.__flags.DEFINE_string("class_data_file", "./data/bh.good3.cls", "Data source for the class list.")
 
     def prepare_data(self):
