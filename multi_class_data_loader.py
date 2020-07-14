176f18079c35c8c11640968a667426466df180f9
MyoungEun Koh <45139611+meunKoh@users.noreply.github.com>
2020-07-14 17:35:51 +0900

    add mecab+sp30k data

--- multi_class_data_loader.py
+++ multi_class_data_loader.py
@@ -24,9 +24,9 @@ class MultiClassDataLoader(object):
 
 
     def define_flags(self):
-        self.__flags.DEFINE_string("train_data_file", "./data/bh.good3.sp30k.train", "Data source for the training data.")
-        self.__flags.DEFINE_string("dev_data_file", "./data/bh.good3.sp30k.dev", "Data source for the cross validation data.")
-        self.__flags.DEFINE_string("test_data_file", "./data/bh.good3.sp30k.test", "Data source for the test data.")
+        self.__flags.DEFINE_string("train_data_file", "./data/bh.good3.mecab+sp30k.train", "Data source for the training data.")
+        self.__flags.DEFINE_string("dev_data_file", "./data/bh.good13.mecab+sp30k.dev", "Data source for the cross validation data.")
+        self.__flags.DEFINE_string("test_data_file", "./data/bh.good13.mecab+sp30k.test", "Data source for the test data.")
         self.__flags.DEFINE_string("class_data_file", "./data/bh.good3.cls", "Data source for the class list.")
 
     def prepare_data(self):
