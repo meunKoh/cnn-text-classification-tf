efa72d7f828226d3fc52e20d24517315e1951e10
MyoungEun Koh <45139611+meunKoh@users.noreply.github.com>
2020-07-27 15:07:39 +0900

    Update text_preprocessing.py

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -46,7 +46,7 @@ class SentencepieceProcessor(object):
         self.max_len = max_len
         self.vocab_size = vocab_size
         self.__tokenizer_name = tokenizer_name
-        self.__tokenizer = self.load_tokenizer()
+        self.tokenizer = self.load_tokenizer()
 
     def load_tokenizer(self):
         if self.__tokenizer_name is None:
@@ -62,41 +62,26 @@ class SentencepieceProcessor(object):
             sp_tokenizer.load(sp_model_path)
             return sp_tokenizer
 
-    def get_tokenizer(self):
-        return self.__tokenizer
-
     def process_datasets(self, datasets):
         return [self.transform(dataset) for dataset in datasets]
 
     def transform(self, data):
         """data : list of string"""
-        return np.array(pad_sequences([self.__tokenizer.encode_as_ids(x) for x in data]
+        return np.array(pad_sequences([self.tokenizer.encode_as_ids(x) for x in data]
                                       , maxlen=self.max_len
                                       , padding='post'
                                       , value=1))
 
 
-class TextProcessor(object):
+class KobertProcessor(object):
 
-    def __init__(self, tokenizer_type, max_len, vocab_size=30000):
-        tokenizer_map = {
-            'mecab': self.__process_with_Tokenizer,
-            'sp': self.__process_with_Sentencepiece,
-            'mesp': self.__process_with_Sentencepiece,
-            'kobert': self.__process_with_kobert
-        }
+    def __init__(self, max_len, tokenizer_name=None, vocab_size=8002):
+        self.max_len = max_len
+        self.vocab_size = vocab_size
+        self.__tokenizer_name = tokenizer_name
+        self.tokenizer = self.load_tokenizer()
 
-        self.__tokenizer_type = tokenizer_type
-        self.__tokenizer = None
-        self.__process_with_tokenizer = tokenizer_map[self.__tokenizer_type]  # data 1개
-        # self.__process_with_tokenizer = tokenizer_map[self.__tokenizer_type]
-        self.__vocab_size = vocab_size
-        self.__max_len = max_len
-
-    def process_texts(self, x_train, y_train, x_dev, y_dev, x_test, y_test):
-        return self.__process_with_tokenizer(x_train, y_train, x_dev, y_dev, x_test, y_test)
-
-    def process_with_kobert(self, x_train, y_train, x_dev, y_dev, x_test, y_test):
+    def load_tokenizer(self):
         import gluonnlp as nlp
         from kobert.utils import get_tokenizer
         from kobert.pytorch_kobert import get_pytorch_kobert_model
@@ -106,19 +91,13 @@ class TextProcessor(object):
         tokenizer = get_tokenizer()
         kobert_tokenizer = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
         transform = nlp.data.BERTSentenceTransform(
-            kobert_tokenizer, max_seq_length=self.__max_len, pad=True, pair=False)
+            kobert_tokenizer, max_seq_length=self.max_len, pad=True, pair=False)
+        return transform
 
-        # transform
-        x_train = self.__process_with_kobert(x_train, transform)
-        x_dev = self.__process_with_kobert(x_dev, transform)
-        x_test = self.__process_with_kobert(x_test, transform)
-        return [x_train, y_train, x_dev, y_dev, x_test, y_test]
+    def process_datasets(self, datasets):
+        return [self.transform(dataset) for dataset in datasets]
 
-    def __process_with_kobert(self, data, transform_fn):
-        data = [transform_fn([sentence])[0] for sentence in data]
+    def transform(self, data):
+        data = [self.tokenizer([sentence])[0] for sentence in data]
         return np.array([array for array in data])
 
-    def clean_data(self, string):
-        if ":" not in string:
-            string = string.strip().lower()
-        return string
